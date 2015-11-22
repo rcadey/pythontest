3b6bfd240e73c8a5daa57834a106699329c5f456
Rob Parkin <robparkin@Robs-Air.home>
2015-11-22 19:55:54 +0000

    adding test .py file

---
+++ test .py
@@ -0,0 +1,17 @@
+# import numpy as numpy
+
+
+def product(x, y):
+    return (x*y)
+
+a = 10
+b = 11
+y = product(a, b)
+print (y)
+print "hello Cadey"
+for i in range(10, 20):
+    print "this is pass", i
+
+print a, b
+# name = raw_input("what is your name")
+# print "your name is", name
